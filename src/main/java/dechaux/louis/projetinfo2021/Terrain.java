1fb8cacc4008e00b4ef335823e7a7ef58456ef09
ldechaux01 <ldechaux01@sec-c210-f14.csip.in>
2021-05-14 08:55:57 +0200

    terrain modif

--- src/main/java/dechaux/louis/projetinfo2021/Terrain.java
+++ src/main/java/dechaux/louis/projetinfo2021/Terrain.java
@@ -10,14 +10,27 @@ package dechaux.louis.projetinfo2021;
  * @author ldechaux01
  */
 public class Terrain {
-    private double p1localisationx;
-    private double p1localisationy;
-    private double p2localisationx;
-    private double p2localisationy;
-    private double p3localisationx;
-    private double p3localisationy;
+    protected double p1localisationx;
+    protected double p1localisationy;
+    protected double p2localisationx;
+    protected double p2localisationy;
+    protected double p3localisationx;
+    protected double p3localisationy;
 
+    public Terrain() {
+        System.out.println("donner les coordonées des 3 point ( x puis y) ");
+        
+        this.p1localisationx = Lire.d();
+        this.p1localisationy = Lire.d();
+        this.p2localisationx = Lire.d();
+        this.p2localisationy = Lire.d();
+        this.p3localisationx = Lire.d();
+        this.p3localisationy = Lire.d();
+    }
+    
+    
     
 public String toString(){
     return "point 1 localisation x :"+this.p1localisationx+"\n point 1 localisation y : "+this.p1localisationy+"\n point 2 localisation x :"+this.p2localisationx+"\n point 2 localisation y : "+this.p2localisationy+"\n point 3 localisation x :"+this.p3localisationx+"\n point 3 localisation y : "+this.p3localisationy;
     }}
+
