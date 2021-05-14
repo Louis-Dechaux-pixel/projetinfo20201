892e936a6a20fea0b7b0966c3769b611cc19d5de
ldechaux01 <ldechaux01@sec-c210-f14.csip.in>
2021-05-14 09:37:47 +0200

    terrain modif

---
+++ src/main/java/dechaux/louis/projetinfo2021/Treilli.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dechaux.louis.projetinfo2021;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import dechaux.louis.projetinfo2021.Lire;
+/**
+ *
+ * @author ldechaux01
+ */
+public class Treilli {
+    
+    protected List<Barre> barrecontenue;
+    protected List<Noeud> noeudcontenue;
+    protected List<Terrain> terraincontenue;
+    protected List<Force> forcecontenue;
+    
+}
