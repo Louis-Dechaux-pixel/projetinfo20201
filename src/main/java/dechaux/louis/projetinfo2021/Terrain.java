17e9b42fbf0c9cfd13cab7d5853e82e06c7e9eda
ldechaux01 <ldechaux01@sec-c210-f14.csip.in>
2021-05-14 08:40:07 +0200

    tostring

--- src/main/java/dechaux/louis/projetinfo2021/Terrain.java
+++ src/main/java/dechaux/louis/projetinfo2021/Terrain.java
@@ -10,9 +10,13 @@ package dechaux.louis.projetinfo2021;
  * @author ldechaux01
  */
 public class Terrain {
-    private double localisationx;
-    private double localisationy;
+    private double p1localisationx;
+    private double p1localisationy;
+    private double p2localisationx;
+    private double p2localisationy;
+    private double p3localisationx;
+    private double p3localisationy;
 
 public String toString(){
-    return "localisation x :"+this.localisationx+"\n localisation y : "+this.localisationy;
+    return "point 1 localisation x :"+this.p1localisationx+"\n point 1 localisation y : "+this.p1localisationy+"\n point 2 localisation x :"+this.p2localisationx+"\n point 2 localisation y : "+this.p2localisationy+"\n point 3 localisation x :"+this.p3localisationx+"\n point 3 localisation y : "+this.p3localisationy;
     }}
