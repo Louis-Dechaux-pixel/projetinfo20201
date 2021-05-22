f7d9e21b718b1a5586dd76da546d859df28d9861
goula <goula@DESKTOP-V22FONL>
2021-05-22 21:52:22 +0200

    sauvegarde

--- src/main/java/dechaux/louis/projetinfo2021/Treilli.java
+++ src/main/java/dechaux/louis/projetinfo2021/Treilli.java
@@ -6,9 +6,7 @@
 package dechaux.louis.projetinfo2021;
 
 import dechaux.louis.projetinfo2021.Barre.Type;
-import java.io.File;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.canvas.GraphicsContext;
@@ -65,5 +63,38 @@ public class Treilli {
     public void setForcecontenue(List<Force> forcecontenue) {
         this.forcecontenue = forcecontenue;
     }
+   public void save(Writer w) throws IOException{ 
+       int b=terraincontenue.size();
+       for(int i=0;i<=b;i++){
+           Terrain te = terraincontenue.get(i);
+           w.append("Triangle;" + te.id + ";(" + te.p1localisationx + "," + te.p1localisationy + ");(" + te.p2localisationx + "," + te.p2localisationy + ");(" + te.p3localisationx + "," + te.p3localisationy+  ")"+ "\n");
+       w.append("FINTRIANGLES");}
+       int c=typebarrecontenue.size();
+       for(int j=0;j<=c;j++){
+           Type ty = typebarrecontenue.get(j);
+           w.append("TypeBarre" + ty.id + ";" + ty.prix + ";"+ ty.longmin + ";" + ty.longmax + ";"+ ty.resmax + ";"+ ty.resmin+ "\n" );
+       w.append("FINCATALOGUES");}
+       int d=noeudcontenue.size();
+       for(int k=0;k<=d;k++){
+           Noeud no = noeudcontenue.get(k);
+           if (no instanceof NoeudSimple){
+             w.append("NoeudSimple"+ no.id + ";"+ no.px +";"+ no.py+"\n");
+           }
+           if (no instanceof Appui){
+             w.append("Appui"+ no.id + ";"+((Appui) no).attache.id +";"+ no.px +";"+ no.py+"\n");  
+           }
+           w.append("FINNOEUDS");}
+       int e=barrecontenue.size();
+       for(int l=0;l<=e;l++){
+           Barre ba = barrecontenue.get(l);
+           w.append("Barre;" + ba.id + ";"+ba.debut.id+":"+ba.fin.id+";"+ba.typebarre.id+"\n");
+       w.append("FINBARRES");
+       }
+   }
+   
+   
+public void sauvegarde(File fout) throws IOException{
+   try(BufferedWriter bout = new BufferedWriter(new FileWriter(fout))){
+       this.save(bout);
+   }    }}
     
-}
