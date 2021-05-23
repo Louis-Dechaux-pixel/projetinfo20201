5f9843f98b0305a17936822d57e1d0da7d87b0f6
goula <goula@DESKTOP-V22FONL>
2021-05-23 19:44:41 +0200

    gui

--- src/main/java/dechaux/louis/projetinfo2021/gui/DessinCanvas.java
+++ src/main/java/dechaux/louis/projetinfo2021/gui/DessinCanvas.java
@@ -39,7 +39,7 @@ public class DessinCanvas extends Pane {
     public void redrawAll() {
         GraphicsContext context = this.realCanvas.getGraphicsContext2D();
         context.setFill(Color.WHITE);
-        context.fillRect(0, 0, this.getWidth(), this.getHeight());
+        context.fillRect(0,50, this.getWidth(), this.getHeight()-100);
         Treilli treilli = this.main.getTreilli();
         
     
